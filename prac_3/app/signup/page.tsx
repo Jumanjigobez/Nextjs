dd45b3671d2cf844cb525805d1276980b8316f45
Jumachaje <73429193+Jumanjigobez@users.noreply.github.com>
2025-07-11 19:18:04 +0000

    Signup page init

---
+++ prac_3/app/signup/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+
+    <>
+    <div className="title">
+        <h1 className="text-4xl text-blue-500 font-bold">Signup</h1>
+    </div>
+
+    <div className="form_part card card-border w-100 p-5 border-blue-500">
+        <form className='card flex flex-col gap-5 w-full'>
+            <div className="input_group flex flex-col gap-2 w-full">
+                <label htmlFor="fname">Full Name</label>
+                <input type="text" name="fname" id="fname" required className='input w-full'/>
+            </div>
+
+             <div className="input_group flex flex-col gap-2 w-full">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" required className='input w-full'/>
+            </div>
+
+            <div className="input_group flex flex-col gap-2 w-full">
+                <label htmlFor="psk">Password</label>
+                <input type="password" name="psk" id="psk" required className='input w-full'/>
+            </div>
+
+            <div className="input_group flex flex-col gap-2 w-full">
+                <label htmlFor="cpsk">Confirm Password</label>
+                <input type="password" name="cpsk" id="cpsk" required className='input w-full'/>
+            </div>
+
+            <div className="input_group">
+               <button type="submit" className='btn w-full bg-blue-500 hover:bg-transparent'>Sign up</button>
+            </div>
+        </form>
+        <div className="input_group mt-5">
+            <button className='btn w-full bg-transparent hover:bg-white-500'>Sign up with Google</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Signup
